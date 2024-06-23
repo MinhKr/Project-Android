8c045bb46c0ff2fb7245108ed474114d0c28baf5
Kien <92088660+KienDuc24@users.noreply.github.com>
2024-06-23 10:51:48 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,14 @@ dependencies {
     implementation(libs.annotation)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
+<<<<<<< Updated upstream
+=======
+    implementation(libs.firebase.auth)
+    implementation("com.hbb20:ccp:2.7.3")
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.database)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+>>>>>>> Stashed changes
     implementation(libs.play.services.maps)
     implementation(libs.play.services.ads)
     implementation(libs.firebase.database)
